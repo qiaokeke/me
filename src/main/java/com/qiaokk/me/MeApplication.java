35b049c75c41521e53f31916fbd84b913b47d40c
qiaokk <qiaokekeshu@163.com>
2017-10-02 20:32:56 +0800

    first

---
+++ src/main/java/com/qiaokk/me/MeApplication.java
@@ -0,0 +1,12 @@
+package com.qiaokk.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/qiaokk/me/domain/Test.java
@@ -0,0 +1,14 @@
+package com.qiaokk.me.domain;
+
+/**
+ * @Author: qiao
+ * @Description:
+ * @Date: Created in 2017/10/2 20:21
+ * @Modified By:
+ * @Email: qiaokekeshu@163.com
+ */
+
+public class Test {
+
+
+}
